b1ccf74e1979a4cab83a44fbbca779173f848d2e
Rémi Achard <remi.achard@ymagis.com>
2018-07-27 16:38:32 +0200

    For some reason, default Ui Loader can't create QTabBar

--- src/widget/uiloader.cpp
+++ src/widget/uiloader.cpp
@@ -22,6 +22,10 @@ QWidget *UiLoader::createWidget(const QString &className, QWidget *parent,
         WaveformWidget *w = new WaveformWidget(parent);
         w->setObjectName(name);
         return w;
+    } else if (className == "QTabBar") {
+        QTabBar *w = new QTabBar(parent);
+        w->setObjectName(name);
+        return w;
     }
 
     return QUiLoader::createWidget(className, parent, name);

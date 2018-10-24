95f91a83d95921e5023192b4128d4210efd786bb
Rémi Achard <remi.achard@ymagis.com>
2018-10-24 14:27:37 +0200

    Improve color / contrast opacity mix
    1D LUT size was reduced from 65535 to 8192
    Computation updated, probably not be the most efficient way

--- src/operator/imageoperator.cpp
+++ src/operator/imageoperator.cpp
@@ -16,8 +16,8 @@ ImageOperator::ImageOperator()
 {
     AddParameter(CheckBoxParameter{ "Enabled", true });
     AddParameter(SliderParameter{ "Opacity", 100.0f, 0.0f, 100.0f, 1.0f });
-    AddParameter(SliderParameter{ "Isolate Contrast", 0.0f, 0.0f, 100.0f, 1.0f });
-    AddParameter(SliderParameter{ "Isolate Color", 0.0f, 0.0f, 100.0f, 1.0f });
+    AddParameter(SliderParameter{ "Contrast", 100.0f, 0.0f, 100.0f, 1.0f });
+    AddParameter(SliderParameter{ "Color", 100.0f, 0.0f, 100.0f, 1.0f });
 
     using std::placeholders::_1;
     Subscribe<UpdateOp>(std::bind(&ImageOperator::OpUpdateParamCallback, this, _1));
@@ -69,54 +69,95 @@ bool ImageOperator::IsIdentity() const
 
 void ImageOperator::Apply(Image & img)
 {
-    float isolate_cts = m_paramList.Get<SliderParameter>("Isolate Contrast").value;
-    float isolate_color = m_paramList.Get<SliderParameter>("Isolate Color").value;
+    float isolate_cts = m_paramList.Get<SliderParameter>("Contrast").value / 100.f;
+    float isolate_color = m_paramList.Get<SliderParameter>("Color").value / 100.f;
 
     const Image img_orig = img;
+    Image img_apply = img;
+    Image img_contrast = img;
+    Image img_color = img;
 
-    if (isolate_cts != 0.0f || isolate_color != 0.0f) {
+    OpApply(img_apply);
 
-        // Compute 1D LUT
-        Image img_1d = Image::Ramp1D(65535, 0.0f, 1.0f, RampType::NEUTRAL);
+    if (isolate_cts != 1.0f || isolate_color != 1.0f) {
+
+        // Compute 1D LUT, ie. the contrast component of the operator
+        uint32_t lut_1d_size = 8192;
+        Image img_1d = Image::Ramp1D(lut_1d_size, 0.0f, 1.0f, RampType::NEUTRAL);
         OpApply(img_1d);
 
         std::ofstream fs("test.spi1d");
-
         fs << "Version 1\n";
         fs << "From 0.000000 1.000000\n";
-        fs << "Length 65535\n";
+        fs << "Length " << lut_1d_size << "\n";
         fs << "Components 3\n";
         fs << "{\n";
 
         float * pix = img_1d.pixels_asfloat();
-        for (uint64_t i = 0; i < 65535; ++i) {
+        for (uint64_t i = 0; i < lut_1d_size; ++i) {
             fs << pix[i * 3] << "\t" << pix[i * 3 + 1] << "\t" << pix[i * 3 + 2] << "\n";
         }
         fs << "}\n";
         fs.close();
 
-        // Apply 1D LUT
+        // Image with contrast only applied
         OCIOFileTransform file_op;
         file_op.SetFileTransform("test.spi1d");
+        file_op.OpApply(img_contrast);
 
-        if (isolate_cts != 0.0f) {
-            float a = isolate_cts / 100.0f;
-            file_op.OpApply(img);
-            img = img_orig * (1.0f - a) + img * a;
+        // Image with color only applied
+        auto direction = file_op.GetParameter<SelectParameter>("Direction");
+        direction.value = "Inverse";
+        file_op.SetParameter(direction);
+        file_op.OpApply(img_color);
+        OpApply(img_color);
+
+        // Mix depending on slider values
+        if (isolate_cts == 1.f && isolate_color == 1.f) {
+            img = img_apply;
         }
-        if (isolate_color != 0.0f) {
-            auto direction = file_op.GetParameter<SelectParameter>("Direction");
-            direction.value = "Inverse";
-            file_op.SetParameter(direction);
-            file_op.OpApply(img);
-            OpApply(img);
+        else if (isolate_cts == 0.f && isolate_color == 0.f) {
+            img = img_orig;
+        }
+        else if (isolate_cts == 0.f || isolate_color == 0.f) {
+            float a = std::max(isolate_cts, isolate_color);
+            img = img_orig * (1.f - a) + img_contrast * isolate_cts + img_color * isolate_color;
+        }
+        else if (isolate_cts == 1.f || isolate_color == 1.f) {
+            float a = std::min(isolate_cts, isolate_color);
+            std::swap(isolate_cts, isolate_color);
+            img = img_apply * a + img_contrast * (1.f - isolate_cts) + img_color * (1.f - isolate_color);
+        }
+        else {
+            const Image *source = nullptr;
+            float a_contrast = 0.0f;
+            float a_color = 0.0f;
 
-            float a = isolate_color / 100.0f;
-            img = img_orig * (1.0f - a) + img * a;
+            // Work backward from destination image
+            if (isolate_cts + isolate_color > 1.f) {
+                source = &img_apply;
+                a_color = 1.f - isolate_cts;
+                a_contrast = 1.f - isolate_color;
+            }
+            // Work forward from source image
+            else {
+                source = &img_orig;
+                a_color = isolate_color;
+                a_contrast = isolate_cts;
+            }
+
+            // qInfo() << "Mix : " << a_source;
+            // qInfo() << "\tContrast slider" << isolate_cts;
+            // qInfo() << "\tColor slider" << isolate_color;
+            // qInfo() << "\tSource" << a_source;
+            // qInfo() << "\tContrast" << a_contrast;
+            // qInfo() << "\tColor" << a_color;
+            float a_source = 1.f - (a_color + a_contrast);
+            img = *source * a_source + img_contrast * a_contrast + img_color * a_color;
         }
     }
     else {
-        OpApply(img);
+        img = img_apply;
     }
 
     float opacity = m_paramList.Get<SliderParameter>("Opacity").value;
